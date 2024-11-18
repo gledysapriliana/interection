6a5023c548afd97371a722a647aa2d0be5093cb0
gledysapriliana <gledysaprill@gmail.com>
2024-11-18 12:57:45 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String actionLabel = 'Belum ada aksi';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+              'Interection'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: (){
+                setState(() {
+                  actionLabel = 'Pengguna melakukan Tap';
+                });
+              },
+              onDoubleTap: (){
+                setState(() {
+                  actionLabel='Pengguna melakukan Tap';
+                });
+              },
+              onLongPress: (){
+                setState(() {
+                  actionLabel='Pengguna melakukan Tap';
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(25),
+                ),
+                child: Center(
+                  child: Text('Aksi',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+            height: 16,
+            ),
+            Text('Pengguna melakukan Tap'),
+          ],
+        ),
+      ),
+    );
+  }
+}
